be2cee05f3f62923ea601f683b7d0448001a20ab
Md Mahadi Hasan <mahadi379377@gmail.com>
2024-07-06 20:20:25 +0600

    largest number

---
+++ Find the largest number.py
@@ -0,0 +1,9 @@
+a = float(input("Enter the first number : "))
+b = float(input("Enter the second number : "))
+c = float(input("Enter the third number : "))
+if a > b and a > c:
+	print(a)
+elif b > a and b > c:
+	print(b)
+else :
+	print(c)
